9e94f297c47e004da51254d888f8fcaca25dd9b2
Seasonal Matcha <seasonalmatcha@gmail.com>
2022-08-23 15:00:05 +0700

    feat: update props and render logic

--- src/components/molecules/Section/index.tsx
+++ src/components/molecules/Section/index.tsx
@@ -3,22 +3,32 @@ import { XMLize, IXMLizeProps } from '../XMLize';
 
 export interface ISectionProps {
   title?: string;
-  subtitle?: string;
+  subtitle?: IXMLizeProps['text'];
   titleProps?: {
     divProps: IXMLizeProps['divProps'];
     textProps: IXMLizeProps['textProps'];
   };
   children: ReactNode;
+  inLineTitle?: IXMLizeProps['inLine'];
 }
 
-export const Section = ({ children, title, subtitle, titleProps }: ISectionProps) => {
+export const Section = ({ children, title, subtitle, titleProps, inLineTitle }: ISectionProps) => {
+  const titleElement = () => {
+    if (typeof title === 'string') {
+      return <h3>{title}</h3>;
+    }
+
+    return title;
+  };
+
   return (
     <section>
       <XMLize
         text={subtitle}
-        title={<h3>{title}</h3>}
+        title={titleElement()}
         divProps={{ className: 'mb-4', ...titleProps?.divProps }}
         textProps={{ className: 'section-title', ...titleProps?.textProps }}
+        inLine={inLineTitle}
       />
       {children}
     </section>
