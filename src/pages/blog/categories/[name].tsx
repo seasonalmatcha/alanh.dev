2fc8ed3c4bc6cc2e752d423baaf86f40e16ae3be
Seasonal Matcha <seasonalmatcha@gmail.com>
2022-08-29 19:15:50 +0700

    refactor: use MainTemplate

--- src/pages/blog/categories/[name].tsx
+++ src/pages/blog/categories/[name].tsx
@@ -1,9 +1,9 @@
-import { AwaitText, PostCard, Section } from '@/components';
+import { AwaitText, MainTemplate, PostCard } from '@/components';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import { trpc } from '@/utils/trpc';
 import { motion } from 'framer-motion';
-import { staggerAnimation } from '@/animations';
+import { fadeUp } from '@/animations';
 import { useEffect, useMemo, useState } from 'react';
 import { useRouter } from 'next/router';
 
@@ -16,22 +16,7 @@ const PostsByCategoryPage: NextPage = () => {
   ]);
   const [fetchingIndex, setFetchingIndex] = useState<number | undefined>(undefined);
 
-  const stagger = useMemo(() => {
-    return staggerAnimation({
-      parent: {
-        show: {
-          transition: {
-            delayChildren: 0.25,
-            staggerChildren: 0.05,
-          },
-        },
-      },
-      children: {
-        hidden: { y: 50 },
-        show: { y: 0 },
-      },
-    });
-  }, []);
+  const fadeAnimation = useMemo(() => fadeUp(), []);
 
   useEffect(() => {
     const handler = (...args: string[]) => {
@@ -53,36 +38,25 @@ const PostsByCategoryPage: NextPage = () => {
         <title>Blog - Alan Habibullah</title>
       </Head>
 
-      <Section
-        subtitle={
-          <motion.h1
-            className="text-4xl font-bold"
-            initial={{ y: -20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-          >
-            Post category: {categoryName}
-          </motion.h1>
-        }
-        inLineTitle
-      >
-        <motion.div
-          className="flex flex-col space-y-8 mt-10"
-          variants={stagger.parent}
-          initial="hidden"
-          animate="show"
-        >
-          {isLoading && (
-            <motion.div variants={stagger.children}>
-              <AwaitText text="myArticles" />
-            </motion.div>
+      <MainTemplate title={`Post category: ${categoryName}`}>
+        <div className="flex flex-col space-y-8 mt-10">
+          {isLoading ? (
+            <AwaitText text="myArticles" />
+          ) : (
+            posts?.map((post, i) => (
+              <motion.div
+                key={i}
+                variants={fadeAnimation}
+                initial="hidden"
+                animate="show"
+                layoutId={`${post.id}`}
+              >
+                <PostCard {...post} bounce={i === fetchingIndex} />
+              </motion.div>
+            ))
           )}
-          {posts?.map((post, i) => (
-            <motion.div key={i} variants={stagger.children} layoutId={`${post.id}`}>
-              <PostCard {...post} bounce={i === fetchingIndex} />
-            </motion.div>
-          ))}
-        </motion.div>
-      </Section>
+        </div>
+      </MainTemplate>
     </>
   );
 };
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,9 @@
-import { AwaitText, Commentize, PostCard, Section } from '@/components';
+import { AwaitText, MainTemplate, PostCard } from '@/components';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import { trpc } from '@/utils/trpc';
-import { motion } from 'framer-motion';
-import { staggerAnimation } from '@/animations';
+import { AnimatePresence, motion } from 'framer-motion';
+import { fadeUp } from '@/animations';
 import { useEffect, useMemo, useState } from 'react';
 import { useRouter } from 'next/router';
 import { FiSearch } from 'react-icons/fi';
@@ -13,23 +13,7 @@ const PostsIndex: NextPage = () => {
   const [query, setQuery] = useState('');
   const { data: posts, isLoading } = trpc.useQuery(['posts.index', { query }]);
   const [fetchingIndex, setFetchingIndex] = useState<number | undefined>(undefined);
-
-  const stagger = useMemo(() => {
-    return staggerAnimation({
-      parent: {
-        show: {
-          transition: {
-            delayChildren: 0.25,
-            staggerChildren: 0.05,
-          },
-        },
-      },
-      children: {
-        hidden: { y: 50 },
-        show: { y: 0 },
-      },
-    });
-  }, []);
+  const fadeAnimation = useMemo(() => fadeUp(), []);
 
   useEffect(() => {
     const handler = (...args: string[]) => {
@@ -51,30 +35,16 @@ const PostsIndex: NextPage = () => {
         <title>Blog - Alan Habibullah</title>
       </Head>
 
-      <Section
-        subtitle={
-          <motion.h1
-            className="text-4xl font-bold"
-            initial={{ y: -20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-          >
-            Blog
-          </motion.h1>
-        }
-        inLineTitle
+      <MainTemplate
+        title="Blog"
+        introDescription={[
+          "I've started writing blog since working at Revival TV",
+          'Frendhi Saido, the VP of Engineering at the said company had encouraged the engineers to do so',
+          "I don't have knowledge of how to write a good articles whatsoever, but Mr. Frendhi said we don't have to worry about the quality, just focus on putting down anything. The quality will get better overtime!",
+          'The focus of writing article is not limited to share the knowledge, but also to help us writer to revisit in the future',
+          "And that's it, I've started to write a little bit and kinda enjoy it",
+        ]}
       >
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.25 } }}>
-          <Commentize
-            text={[
-              "I've started writing blog since working at Revival TV",
-              'Frendhi Saido, the VP of Engineering at the said company had encouraged the engineers to do so',
-              "I don't have knowledge of how to write a good articles whatsoever, but Mr. Frendhi said we don't have to worry about the quality, just focus on putting down anything. The quality will get better overtime!",
-              'The focus of writing article is not limited to share the knowledge, but also to help us writer to revisit in the future',
-              "And that's it, I've started to write a little bit and kinda enjoy it",
-            ]}
-          />
-        </motion.div>
-
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, transition: { delay: 0.25 } }}
@@ -88,24 +58,26 @@ const PostsIndex: NextPage = () => {
           <FiSearch className="absolute top-1/2 left-2 -translate-y-1/2 w-5 h-5" />
         </motion.div>
 
-        <motion.div
-          className="flex flex-col space-y-8 mt-10"
-          variants={stagger.parent}
-          initial="hidden"
-          animate="show"
-        >
-          {isLoading && (
-            <motion.div variants={stagger.children}>
+        <div className="flex flex-col space-y-8 mt-10">
+          <AnimatePresence mode="wait">
+            {isLoading ? (
               <AwaitText text="myArticles" />
-            </motion.div>
-          )}
-          {posts?.map((post, i) => (
-            <motion.div key={i} variants={stagger.children} layoutId={`${post.id}`}>
-              <PostCard {...post} bounce={i === fetchingIndex} />
-            </motion.div>
-          ))}
-        </motion.div>
-      </Section>
+            ) : (
+              posts?.map((post, i) => (
+                <motion.div
+                  key={i}
+                  variants={fadeAnimation}
+                  initial="hidden"
+                  animate="show"
+                  layoutId={`${post.id}`}
+                >
+                  <PostCard {...post} bounce={i === fetchingIndex} />
+                </motion.div>
+              ))
+            )}
+          </AnimatePresence>
+        </div>
+      </MainTemplate>
     </>
   );
 };
