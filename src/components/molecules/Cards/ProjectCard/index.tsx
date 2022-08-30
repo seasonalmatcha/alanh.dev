918d9caffd7b6ea7a1dd138a2e29d5f001d3b3d3
Seasonal Matcha <seasonalmatcha@gmail.com>
2022-08-30 22:18:28 +0700

    refactor: types

--- src/components/molecules/Cards/ProjectCard/index.tsx
+++ src/components/molecules/Cards/ProjectCard/index.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image';
-import { Commentize } from '@/components';
-import { IProject } from '@/types';
+import { Commentize, ICommentizeProps } from '@/components';
+import { Project } from '@prisma/client';
 
-export const ProjectCard = ({ description, href, thumbnail, title }: IProject) => {
+type IProjectCardProps = Pick<Project, 'id' | 'href' | 'thumbnail' | 'title'> & {
+  description: ICommentizeProps['text'];
+};
+
+export const ProjectCard = ({ description, href, thumbnail, title }: IProjectCardProps) => {
   return (
     <div>
       <a href={href} target="_blank" rel="noreferrer" className="project-card-thumbnail link">
