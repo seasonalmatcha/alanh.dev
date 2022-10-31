1cae5773d5018a0667ede24a5a0a8f20d528d37d
Seasonal Matcha <seasonalmatcha@gmail.com>
2022-10-31 10:21:13 +0700

    fix: add order by createdAt for experience

--- src/server/router/experiences-router.ts
+++ src/server/router/experiences-router.ts
@@ -6,7 +6,13 @@ import { experienceSchema } from '@/schemas';
 export const experiencesRouter = createRouter()
   .query('index', {
     async resolve({ ctx }) {
-      const data = await ctx.prisma.experience.findMany();
+      const data = await ctx.prisma.experience.findMany({
+        orderBy: [
+          {
+            createdAt: 'desc',
+          },
+        ],
+      });
 
       return data.map((project) => ({
         ...project,
