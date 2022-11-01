4530109636193490210325e80b131aaeca864431
Seasonal Matcha <seasonalmatcha@gmail.com>
2022-11-02 02:43:47 +0700

    feat: add metatags on contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,11 +1,22 @@
 import { NextPage } from 'next';
 import { ContactForm } from '@/components';
+import Head from 'next/head';
+import { generateMetatags } from '@/utils/generateMetatags';
 
 const ContactPage: NextPage = () => {
   return (
-    <div className="max-w-xl mx-auto">
-      <ContactForm />
-    </div>
+    <>
+      <Head>
+        {generateMetatags({
+          title: 'Contact - Alan Habibullah',
+          url: '/contact',
+        })}
+      </Head>
+
+      <div className="max-w-xl mx-auto">
+        <ContactForm />
+      </div>
+    </>
   );
 };
 
