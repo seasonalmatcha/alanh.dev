e88dffc4d5cd75cdc4bca3e8c07855587c9c9bfa
Seasonal Matcha <seasonalmatcha@gmail.com>
2023-03-14 00:37:04 +0700

    feat: change card wrapper display to block
    So the outline covers the whoe card/section

--- src/components/molecules/Cards/SnippetCard/index.tsx
+++ src/components/molecules/Cards/SnippetCard/index.tsx
@@ -31,7 +31,7 @@ export const SnippetCard = ({
   }, [bounce, controls]);
 
   return (
-    <Link href={`/snippets/${slug}`} passHref>
+    <Link href={`/snippets/${slug}`} passHref className="block">
       <motion.div animate={controls} className="snippet-card group">
         <div className="card-body">
           {logo && (
